91349031d7fb542634bd9ca6ca0a88a01161b926
Edwin <edwin.h.morris@gmail.com>
2015-03-29 00:57:02 -0400

    Site updated to ab678dc

---
+++ javascripts/mainNavigation-sections-link--currentPage.js
@@ -0,0 +1,1 @@
+(function(){$(function(){return $(".mainNavigation-sections-link").map(function(){return this.href===window.location.href?this:void 0}).addClass("mainNavigation-sections-link--currentPage")})}).call(this);
